1824f88ea70ce9f9a1a3be3c8d803eb4e46da84c
Donny Malik Kurniawan <bossdony@gmail.com>
2023-08-27 20:38:19 +0700

    Database master 27 Agustus 2023

---
+++ dev/display_content_2023.08.27.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 27 Agu 2023 pada 13.37
+-- Versi server: 8.0.31
+-- Versi PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `display_content`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin_outlet`
+--
+
+DROP TABLE IF EXISTS `admin_outlet`;
+CREATE TABLE IF NOT EXISTS `admin_outlet` (
+  `id_outlet` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_outlet` varchar(64) NOT NULL,
+  `alamat_outlet` varchar(255) NOT NULL,
+  `kota` varchar(64) NOT NULL,
+  `foto_outlet` varchar(255) DEFAULT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_outlet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int NOT NULL AUTO_INCREMENT,
+  `nama_client` varchar(128) NOT NULL,
+  `id_outlet` int NOT NULL,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `content`
+--
+
+DROP TABLE IF EXISTS `content`;
+CREATE TABLE IF NOT EXISTS `content` (
+  `id_content` int NOT NULL AUTO_INCREMENT,
+  `jenis_content` enum('gambar','video') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `screen_orientation` enum('landscaoe','portrait') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_content` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `data` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_outlet` int NOT NULL,
+  `aktif` tinyint(1) NOT NULL COMMENT '1=aktif, 0=nonaktif',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_content`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `superadmin`
+--
+
+DROP TABLE IF EXISTS `superadmin`;
+CREATE TABLE IF NOT EXISTS `superadmin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_superadmin` varchar(32) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Superadmin yang mengelola admin outlet';
+
+--
+-- Dumping data untuk tabel `superadmin`
+--
+
+INSERT INTO `superadmin` (`id`, `username`, `password`, `nama_superadmin`, `timestamp`) VALUES
+(1, 'ryzal', '$2a$10$IcyXMoEaj6ulIkgJ58gxYO0/gVhrLzudVOuBNgZgRlPsaEuf9pKCy', 'Ryzal', '2023-08-19 13:48:46');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
